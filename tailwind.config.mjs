7e7309a3122981e1f03ffdbc66ffc9b7aa9c802c
Jes015 <jes.18.sin.15@gmail.com>
2024-02-04 20:37:07 -0500

    chore(migration): add text field component (#1)
    resolves #1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		{
+			extend: {
+			  colors: {
+				borderPrimary: '#262626',
+				backgroundHoverPrimary: '#171717',
+				textPrimary: '#eceeed',
+				textSecondary: '#d4d4d4',
+				textTertiary: '#a3a3a3'
+			  }
+			},
+		  },
 	},
 	plugins: [],
 }
